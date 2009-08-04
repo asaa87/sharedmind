4fd714567c5d0a665bae80373ed84f31bc3faa21
asaa87 <asaa87@53cd23c0-f43b-11dd-984d-ab748d24aa7e>
2009-08-04 06:42:04 +0000

    Fix conflict window hang

--- plugins/ConflictWindow.java
+++ plugins/ConflictWindow.java
@@ -4,9 +4,18 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class ConflictWindow {
-    public ConflictWindow(JFrame frame) {
-        JOptionPane.showMessageDialog(frame,
+    public ConflictWindow(final JFrame frame) {
+    	java.awt.EventQueue.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+		        JOptionPane.showMessageDialog(frame,
                         "There's a conflicting change.", 
                         "Conflict detected", JOptionPane.WARNING_MESSAGE);
+			}
+    		
+    	});
+
     }
 }
