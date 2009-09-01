9f96134afbdb10743de0fda6162ed9310fbc86a3
asaa87 <asaa87@53cd23c0-f43b-11dd-984d-ab748d24aa7e>
2009-09-01 09:39:34 +0000

    Add abort connection button

--- trunk/plugins/sharedmind/ConnectingWindow.java
+++ trunk/plugins/sharedmind/ConnectingWindow.java
@@ -1,13 +1,24 @@
 package plugins.sharedmind;
 
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 public class ConnectingWindow {
-	MapSharingController mpc;
-	JDialog window;
-	
+	private MapSharingController mpc;
+	private JDialog window;
+	private JButton abortButton;
+	private JPanel panel;
+	private JPanel panel2;
+	private JPanel panel3;
+	private JLabel label;
 	
     public ConnectingWindow(JFrame frame, MapSharingController mpc) {
     	System.out.println("show connecting window");
@@ -17,6 +28,31 @@ public class ConnectingWindow {
     	this.window.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
     	this.window.setTitle("Connecting ...");
     	this.window.setSize(300, 100);
+    	
+    	this.abortButton = new JButton("Abort");
+		final MapSharingController mpc_copy = mpc;
+    	this.abortButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				mpc_copy.unsubscribeToTopic();
+				mpc_copy.hideConnectingWindow();
+			}
+    		
+    	});
+    	
+    	this.label = new JLabel("Connecting ... Please wait ...");
+    	
+    	this.panel2 = new JPanel();
+    	this.panel2.add(this.label);
+    	this.panel3 = new JPanel();
+    	this.panel3.add(this.abortButton);
+    	
+    	this.panel = new JPanel();
+    	this.panel.setLayout(new GridLayout(2, 1));
+    	this.panel.add(this.panel2);
+    	this.panel.add(this.panel3);
+    	this.window.add(this.panel);
+    	
         final JDialog window = this.window;
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
