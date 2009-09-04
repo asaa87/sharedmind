cb17540903f2d85cb7d54287aed291665a28ba8b
asaa87 <asaa87@53cd23c0-f43b-11dd-984d-ab748d24aa7e>
2009-09-04 04:18:38 +0000

    Consistent ordering of new node

--- plugins/sharedmind/SynchronousEditingHistory.java
+++ plugins/sharedmind/SynchronousEditingHistory.java
@@ -1,6 +1,7 @@
 package plugins.sharedmind;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Vector;
 
 import org.apache.log4j.Logger;
@@ -11,8 +12,10 @@ import freemind.controller.actions.generated.instance.DeleteNodeAction;
 import freemind.controller.actions.generated.instance.EditNodeAction;
 import freemind.controller.actions.generated.instance.NewNodeAction;
 import freemind.controller.actions.generated.instance.NodeAction;
+import freemind.controller.actions.generated.instance.PasteNodeAction;
 import freemind.controller.actions.generated.instance.XmlAction;
 import freemind.modes.NodeAdapter;
+import freemind.modes.mindmapmode.actions.xml.ActionPair;
 
 /**
  * 
@@ -21,7 +24,8 @@ import freemind.modes.NodeAdapter;
  *
  */
 public class SynchronousEditingHistory {
-    private Vector<SharedAction> history;
+    private static final PasteNodeAction PasteNodeAction = null;
+	private Vector<SharedAction> history;
     private HashMap<String, SharedAction> last_action_of_participants;
     private MapSharingController mpc;
     private Logger log;
@@ -193,5 +197,89 @@ public class SynchronousEditingHistory {
             }
         }
 		return false;
+	}
+	
+	/*
+	 * Returns the actions that needed to be undone before the shared action is executed 
+	 * and redone after that. These actions are:
+	 * 1. Concurrent actions of delete and new node, where the node deleted is the sibling of new node.
+	 * 2. Concurrent new node actions, where the parent of these new nodes are the same.
+	 */
+	public synchronized Vector<SharedAction> getFollowingActions(SharedAction shared_action) {
+		Vector<SharedAction> return_value = new Vector<SharedAction>();
+		ActionPair action_pair = shared_action.getActionPair();
+		
+		if (action_pair.getDoAction() instanceof NodeAction ||
+				action_pair.getDoAction() instanceof CompoundAction) {
+			
+			// Get list of actions in history that might need to be undone first.
+			int i;
+			for (i = 0; i < history.size(); ++i){
+				if (this.history.get(i).compareTo(shared_action) > 0) {
+					break;
+				}	
+			}
+			Vector<SharedAction> following_actions = 
+					new Vector<SharedAction>(this.history.subList(i, this.history.size()));
+			
+			HashMap<String, String> new_nodes = new HashMap<String, String>();
+			if (action_pair.getDoAction() instanceof NewNodeAction) {
+				NewNodeAction new_node = (NewNodeAction) action_pair.getDoAction();
+				new_nodes.put(new_node.getNewId(), new_node.getNode());
+			}
+			for (SharedAction action : following_actions) {
+				XmlAction do_action = action.getActionPair().getDoAction();
+				if (do_action instanceof NewNodeAction) {
+					NewNodeAction new_node = (NewNodeAction) do_action;
+					new_nodes.put(new_node.getNewId(), new_node.getNode());
+				}
+			}
+			
+			for (SharedAction action : following_actions) {
+				if (needUndo(action.getActionPair(), new_nodes)) {
+					return_value.add(action);
+				}
+			}
+		}
+		return return_value;
+	}
+	
+	private boolean needUndo(ActionPair pair, HashMap<String, String> new_nodes) {
+		// Get list of parents
+		Vector<String> parent_list = getListOfParents(pair);
+		
+		for (String parent : parent_list) {
+			if (new_nodes.containsValue(parent))
+				return true;
+		}
+		
+		if (parent_list.size() == 0 && pair.getDoAction() instanceof NodeAction) {
+			NodeAction node_action = (NodeAction) pair.getDoAction();
+			if (new_nodes.containsKey(node_action.getNode()))
+				return true;
+		}
+		return false;
+	}
+	
+	private Vector<String> getListOfParents (ActionPair pair) {
+		Vector<String> return_value = new Vector<String>();
+		
+		if (pair.getDoAction() instanceof NewNodeAction) {
+			NewNodeAction do_action = (NewNodeAction) pair.getDoAction();
+			return_value.add(do_action.getNode());
+		} else if (pair.getDoAction() instanceof DeleteNodeAction ||
+				pair.getDoAction() instanceof CutNodeAction) {
+			PasteNodeAction undo_action = (PasteNodeAction) pair.getUndoAction();
+			return_value.add(undo_action.getNode());
+		} else if (pair.getDoAction() instanceof CompoundAction &&
+				((CompoundAction) pair.getDoAction()).getChoice(0) instanceof CutNodeAction) {
+			List<PasteNodeAction> undo_actions = 
+				((CompoundAction) pair.getUndoAction()).getListChoiceList();
+			for (PasteNodeAction undo_action : undo_actions) {
+				return_value.add(undo_action.getNode());
+			}
+		}
+		
+		return return_value;
 	}
 }
